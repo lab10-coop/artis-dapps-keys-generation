b249e97b566a20158e4df1f60abc3aacf540e606
didi <git@d10r.net>
2018-12-13 11:27:34 +0100

    disabled service worker in order to avoid 'old version from cache' issues

--- src/index.js
+++ src/index.js
@@ -6,4 +6,7 @@ window.addEventListener('beforeunload', function(event) {
   event.returnValue = 'Are you sure?'
 })
 ReactDOM.render(<App generateKeysIsDisabled={true} />, document.getElementById('root'))
-registerServiceWorker()
+
+// service worker disabled because we don't yet understand how exactly it affects caching policy
+// and want to avoid further issues caused by old versions of files served from cache
+//registerServiceWorker()
