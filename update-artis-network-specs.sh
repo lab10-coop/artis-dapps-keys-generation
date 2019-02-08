fb00ed8b34e41f2001051a8f79500b4dfa797084
Didi <dho@lab10.coop>
2019-02-08 19:56:07 +0100

    fixed dependent git paths

--- update-artis-network-specs.sh
+++ update-artis-network-specs.sh
@@ -5,8 +5,8 @@ cd ./public/networks
 rm -r sigma1;
 rm -r tau1;
 
-git clone git@code.lab10.io:graz/04-artis/poa/poa-chain-spec.git sigma1 -b sigma1 
-git clone git@code.lab10.io:graz/04-artis/poa/poa-chain-spec.git tau1 -b tau1
+git clone https://github.com/lab10-coop/artis-chain-spec.git sigma1 -b sigma1
+git clone https://github.com/lab10-coop/artis-chain-spec.git tau1 -b tau1
 
 #remove GIT repo, so git does not see it as git repositories.
 
